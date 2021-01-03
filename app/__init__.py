88b980180a782958a5cd431fe7b357b2e4183be2
Pablo <pabdom03@ucm.es>
2021-01-03 14:12:53 +0100

    na funciona

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,20 @@ from flask import Flask, request
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from flask_session import Session
+
+SESSION_TYPE = 'memcache'
 
 app = Flask(__name__)
+sess = Session()
+
 app.static_folder = 'static'
 app.template_folder = "templates"
 
+
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
+sess.init_app(app)
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, Markup
+from flask import render_template, request, Markup, flash, get_flashed_messages
 from app import app
+import re
 
 @app.route('/')
 @app.route('/index')
@@ -13,5 +14,9 @@ def build_grid():
     svg = './static/img/calculating.svg'
     #print("rows: {}".format(rows))
     #print("cols: {}".format(cols))
-
-    return render_template('index.html', rows=rows, cols=cols,svg=svg)
+    t = len(re.findall("[^0-9]",rows))+len(re.findall("[^0-9]",cols))
+    if t > 0:
+        flash(('Please enter only numbers',"block"))
+        return render_template('index.html', rows=rows, cols=cols,svg=svg)
+    else:
+        return render_template('index.html', rows=rows, cols=cols,svg=svg)
