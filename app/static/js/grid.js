d4874c9dfad5b398f5c59b0a598beb2853dfb455
Pablo <pabdom03@ucm.es>
2021-01-05 03:42:51 +0100

    javascript tiles in progress

--- app/static/js/grid.js
+++ app/static/js/grid.js
@@ -1,6 +1,13 @@
 
-$(document).ready(function() {
-    $('#box').click(function() {
-        $(this).find(".error-message").toggleClass('open');
-    });
-});
+
+var horiz = document.getElementsByClassName("seven columns")[0].offsetWidth;
+
+var vert =  document.getElementsByClassName("seven columns")[0].offsetHeight;
+
+
+function create_grid(nrows, ncols) {
+    var quotient = Math.floor(ncols/horiz);
+    return p1 * p2;   // The function returns the product of p1 and p2
+}
+
+// TODO
