f95416b8a79d379150d6f2c72777e89ca5b83346
Pablo <pabdom03@ucm.es>
2021-01-06 10:00:22 +0100

    building rows

--- app/static/js/grid.js
+++ app/static/js/grid.js
@@ -1,13 +1,38 @@
 
 
-var horiz = document.getElementsByClassName("seven columns")[0].offsetWidth;
+// Call the below function
+// waitForElementToDisplay(".seven-columns",function(){alert("Hi");},1000,9000);
+/*
+document.addEventListener("DOMContentLoaded", function(event) {
+    
+});*/
 
-var vert =  document.getElementsByClassName("seven columns")[0].offsetHeight;
 
 
-function create_grid(nrows, ncols) {
-    var quotient = Math.floor(ncols/horiz);
-    return p1 * p2;   // The function returns the product of p1 and p2
-}
-
-// TODO
+jQuery(document).ready(function() {
+    var temp = document.getElementsByClassName("seven columns");
+    horiz = temp[0].offsetWidth;
+    vert = temp[0].offsetHeight;
+    function create_grid(nrows, ncols) {
+        var grid_cols_size = Math.floor(horiz/ncols);
+        var cols_scale = grid_cols_size/tile_width;
+        var tile_width = document.getElementsByClassName("tile-small")[0].offsetWidth;
+        
+        // we write the elements
+        for (i = 0; i < ncols; i++) { //posiblemente de error por el tipo de ncols
+            // create a new div element
+            const newDiv = document.createElement("div");
+            
+            // setAttributes
+            newDiv.setAttribute("data-role", "tile");
+            newDiv.setAttribute("data-size", "small");
+            newDiv.style.transform = "scale("+cols_scale.toString() +") !important";
+            
+            // add it
+            const currentDiv = document.getElementById("main-row");
+            currentDiv.appendChild(newDiv);
+        }
+    }
+    // add it to global scope
+    window.create_grid = create_grid;
+});
